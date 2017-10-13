dd86b5a8e4c2e8ca19021ce5855a24afd5dec035
wangjie9 <991036184@qq.com>
2017-10-13 12:35:14 +0800

    界面优化

--- src/pages/userpage/settings/settings.ts
+++ src/pages/userpage/settings/settings.ts
@@ -1,22 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {AboutusPage} from "../aboutus/aboutus";
-
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-seting',
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alerCtrl: AlertController) {
   }
   aboutUs(){
     this.navCtrl.push(AboutusPage);
   }
-//  app.controller('MessageCtrl', function ($scope, $state, $window) {
-//  $scope.call = function (phone) {
-//    $window.location.href = "tel:" + phone;
-//  }
-//});
+  clear() {
+    let confirm = this.alerCtrl.create({
+      title: '确定清除缓存吗?',
 
+      buttons: [
+        {
+          text: '取消',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: '确认',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
+  }
+  upVersion() {
+    let alert = this.alerCtrl.create({
+      title: '当前为最新版本',
+      //message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
+      buttons: ['确认']
+    });
+    alert.present()
+  }
 }
--- src/pages/userpage/user.ts
+++ src/pages/userpage/user.ts
@@ -3,29 +3,41 @@ import { NavController } from 'ionic-angular';
 import  { UserDetailsPage } from "./userdetail/userDetails";
 import {SettingsPage} from "./settings/settings";
 
-//import { AlertController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 @Component({
   selector: 'page-user',
   templateUrl: 'user.html'
 })
 export class UserPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alerCtrl: AlertController) {
 
   }
-
   intoUserDetails(){
     this.navCtrl.push(UserDetailsPage);
   }
   settings(){
     this.navCtrl.push(SettingsPage);
   }
-  //quitLog(){
-  //  let alert = this.alert.create({
-  //    title: '确定退出吗?',
-  //    //message: 'Your friend, Obi wan Kenobi, just approved your friend request!',
-  //    buttons: ['确定']
-  //  });
-  //  alert.present()
-  //}
+  quitLog() {
+    let confirm = this.alerCtrl.create({
+      title: '确认退出吗?',
+
+      buttons: [
+        {
+          text: '取消',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: '确认',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present()
+  }
 }
