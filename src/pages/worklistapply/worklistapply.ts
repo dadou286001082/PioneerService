26bbf8d75a02a124895a06967930a4fac0cb185b
wangjie9 <991036184@qq.com>
2017-11-01 17:34:15 +0800

    修改工单申请

--- src/pages/worklistapply/worklistapply.ts
+++ src/pages/worklistapply/worklistapply.ts
@@ -6,9 +6,12 @@ import {AppService} from "../../providers/app.service";
 import { Camera, CameraOptions } from '@ionic-native/camera';//拍照
 // import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';//文件
 import { ImagePicker } from '@ionic-native/image-picker';//读取本地照片库
-import { ActionSheetController } from "ionic-angular";
+import { ActionSheetController,Platform } from "ionic-angular";
 import { AlertController } from 'ionic-angular';
 import {Loginstate} from "../login/loginstate";
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import {File} from "@ionic-native/file";
+import { APP_SERVE_URL} from "../../providers/app.global";
 
 @Component({
   selector: 'page-contact',
@@ -24,7 +27,8 @@ export class WorklistapplyPage {
   image;//上传图片参数
   imageBase;//每个图片url
   work;//申请工单请求参数
-   images=[];//照片数组
+  images=[];//照片数组
+
   provinceList=[];//省份&id列表
   capital:string;//选择框选中省份
   carnumbser:string;//用户输入的车牌号
@@ -32,12 +36,20 @@ export class WorklistapplyPage {
   workList=[];//工单类型列表
   works:string;//选择框选中工单类型
   applyReason:string;//工单申请原因
+  imgone;//第一张图片
 //调用拍照参数
-  options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
+  private options= {
+    quality: 50,
+    destinationType: 1, // Camera.DestinationType.FILE_URI,
+    sourceType: 1, // Camera.PictureSourceType.CAMERA,
+    encodingType: 0, // Camera.EncodingType.JPEG,
+    mediaType: 0, // Camera.MediaType.PICTURE,
+    allowEdit: true,
+    correctOrientation: true
+    // quality: 100,
+    // destinationType: this.camera.DestinationType.FILE_URI,
+    // encodingType: this.camera.EncodingType.JPEG,
+    // mediaType: this.camera.MediaType.PICTURE
   }
   // 调用相册时传入的参数
   private imagePickerOpt = {
@@ -48,29 +60,31 @@ export class WorklistapplyPage {
   };
   constructor(public navCtrl: NavController,public userParticulars:UserParticulars,public appService:AppService,
               public  camera:Camera,private actionSheetCtrl: ActionSheetController,public alerCtrl: AlertController,
-              public imagePicker :ImagePicker,public loginstate:Loginstate
-              ) {
+              public imagePicker :ImagePicker,public loginstate:Loginstate,public platform:Platform,private transfer: FileTransfer,
+              public file:File
+  ) {
 
   }
-
+  fileTransfer: FileTransferObject = this.transfer.create();
 
 
 //工单申请
   workOk(){
-  console.log("省份"+this.capital+this.carnumbser+this.works+this.applyReason);
-    this.images.push( );
-  this.work={
-    province_code_id:this.capital,
-    plate_no:this.carnumbser,
-    work_sheet_category_id:this.works,
-    work_sheet_description:this.applyReason,
-    image_list:this.images
-  };
+    console.log("省份"+this.capital+this.carnumbser+this.works+this.applyReason);
+    // this.images.push('http://218.244.158.175/xauto_poineer_server/image/catalog/work_sheet_image/work_sheet_image_c7975fa02.jpg');
+
+    this.work={
+      province_code_id:this.capital,
+      plate_no:this.carnumbser,
+      work_sheet_category_id:this.works,
+      work_sheet_description:this.applyReason,
+      image_list:this.images
+    };
     this.jsonText={
       user_id:this.loginstate.loginUserId,
       work_sheet_info:this.work
 
-  }
+    }
     this.params ={
       route:'work_sheet/work_sheet/applyWorkSheet',
       token:this.loginstate.token,
@@ -82,11 +96,11 @@ export class WorklistapplyPage {
       if(d.status['succeed']==1){
 
         this.appService.toast("工单申请成功");
-
+        this.appService.alert(JSON.stringify(this.jsonText));
       }else {
-        this.appService.toast("工单申请成功");
+        this.appService.toast("工单申请失败");
       }
-      this.provinceList=d.data['province_code_list'];
+
     },true)
 
 
@@ -101,7 +115,7 @@ export class WorklistapplyPage {
     }
     this.appService.httpPost(this.params,d=>{
       console.log(JSON.stringify(d));
-        this.provinceList=d.data['province_code_list'];
+      this.provinceList=d.data['province_code_list'];
     },true)
 
     this.params ={
@@ -109,7 +123,7 @@ export class WorklistapplyPage {
     }
     this.appService.httpPost(this.params,d=>{
       console.log(JSON.stringify(d));
-        this.workList=d.data['work_sheet_category_list'];
+      this.workList=d.data['work_sheet_category_list'];
     },true)
 
 
@@ -119,47 +133,58 @@ export class WorklistapplyPage {
 
 //拍照第一张
   photo1(){
-    let confirm = this.alerCtrl.create({
-      // title: '请选择',
-
+    let actionSheet = this.actionSheetCtrl.create({
+      // title: 'Albums',
+      cssClass: 'action-sheets-basic-page',
       buttons: [
         {
           text: '拍照',
+          // role: 'destructive',
+          // icon: !this.platform.is('ios') ? 'trash' : null,
           handler: () => {
-            console.log('Disagree clicked');
             console.log("拍照第一张");
-            this.camera.getPicture(this.imagePickerOpt).then((imageData) => {
+
+            this.camera.getPicture(this.options).then((imageData) => {
               // imageData is either a base64 encoded string or a file URI
               // If it's base64:
-              this.imageBase = 'data:image/jpeg;base64,' + imageData;
-
-            this.upPhoto();
+              this.appService.alert("拍照"+imageData);
+              // this.imageBase = 'data:image/jpeg;base64,' + imageData;
+              this.imageBase = imageData;
+              this.upPhoto();
             }, (err) => {
               // Handle error
             });
           }
         },
         {
-          text: '相册',
+          text: '从相册选取',
+          // icon: !this.platform.is('ios') ? 'share' : null,
           handler: () => {
-            this.imagePicker.getPictures(this.options).then((results) => {
+            console.log('从相册选取第一张');
+            this.imagePicker.getPictures(this.imagePickerOpt).then((results) => {
               for (var i = 0; i < results.length; i++) {
                 console.log('Image URI: ' + results[i]);
               }
             }, (err) => { });
-
-            console.log('Agree clicked');
           }
-        }
+        },
+        {
+          text: '取消',
+          // icon: !this.platform.is('ios') ? 'arrow-dropright-circle' : null,
+          handler: () => {
+            console.log('取消');
+          }
+        },
+
+
       ]
     });
-    confirm.present()
-
-
+    actionSheet.present();
 
   }
-
+//上传图片
   upPhoto(){
+    this.appService.alert("上传图片:"+this.imageBase);
     this.jsonText={
       image_type:'16',
 
@@ -167,20 +192,59 @@ export class WorklistapplyPage {
     this.params ={
       route:'base/tools/editImage',
       jsonText:JSON.stringify(this.jsonText),
-      image:this.imageBase
+      // image:this.imageBase
+      device_type: "20", //需要替换成 android=20 || ios=10
+      device_version: "1.0", //需要替换成 当前版本号
+      version_code: "1", //需要替换成 当前版本号
+      channel: "20001_website",
     }
-    this.appService.httpPost(this.params,d=>{
-      console.log(JSON.stringify(d));
-      this.images.push( d.data.image_info['thumb']);
 
-    },true)
+    let options: FileUploadOptions = {
+      fileKey: 'image',
+      fileName: 'name.jpg',
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' //不加入 发生错误！！
+      },
+      params:this.params
+    }
+
+    this.fileTransfer.upload(this.imageBase, encodeURI(APP_SERVE_URL)
+      , options)
+      .then((data) => {
+        this.appService.alert('上传成功返回：'+JSON.stringify(data));
+
+        //照片添加到数组
+        // this.appService.alert('上传成功返回2：'+JSON.parse(data.response['data']['image_info']));
+        // this.images.push(data.response['data']);
+        // this.images.push(data.response['data']);
+        this.appService.alert('上传成功返回1：'+JSON.stringify(JSON.parse(data.response)));
+        this.appService.alert('上传成功返回2：'+JSON.parse(JSON.stringify(JSON.parse(data.response))).data);
+
+      }, (err) => {
+        // error
+        this.appService.alert('上传失败返回：'+JSON.stringify(err));
+      })
+
+    // this.appService.httpPost(this.params,d　=>{
+    //   // console.log(JSON.stringify(d));
+    //   // if(d.status.succeed ==1){
+    //     // console.log("图片上传成功");
+    //     // this.images.push( d.data.image_info['thumb']);
+    //     this.appService.alert("图片上传成功返回值为："+ JSON.stringify(d));
+    //     // this.imageBase = d.data.image_info['thumb']
+    //     // document.getElementById('img1');
+    //
+    //   // }
+    //
+    //
+    // },true)
   }
 
   /**
    * 进入个人中心
    */
   onUsers(){
-this.navCtrl.push(UserPage)
+    this.navCtrl.push(UserPage)
   }
 
 
